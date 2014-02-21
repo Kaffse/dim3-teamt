5fdf7ae3eb71535827cb0c26c6367fd736fb501e
Ally Weir <ally.pcgf@gmail.com>
2014-02-21 16:33:27 +0000

    Added run script. Updated venvusage, generated requirements.txt

--- dim3app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from dim3app.models import Project
-from dim3app.models import User
-from dim3app.models import Task
-
-admin.site.register(Project)
-admin.site.register(User)
-admin.site.register(Task)
